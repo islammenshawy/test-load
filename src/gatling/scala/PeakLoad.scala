1d32fdf08146cc04d0a0f833bb2532741b85130c
samanthastilson <sstilson@thoughtworks.com>
2017-08-29 15:16:09 -0700

    [FND-16] Search load test looks for element in response from bloomreach [Jayson/Sam]

--- src/gatling/scala/PeakLoad.scala
+++ src/gatling/scala/PeakLoad.scala
@@ -12,7 +12,8 @@ class PeakLoad extends Simulation {
   val scn = scenario("PeakLoad")
     .group("Request") {
       exec(http("search page")
-        .get(session => "/api/v1/core/?account_id=6063&auth_key=&domain_key=oldnavy&request_id=7839579868130&_br_uid_2=uid=7596393004555:_uid=5727955944386:v=11.8:ts=1480613947786:hc=108&url=www.bloomique.com&ref_url=www.bloomique.com&request_type=search&rows=200&start=0&facet.limit=300&fl=pid,title,brand,price,sale_price,promotions,thumb_image,sku_thumb_images,sku_swatch_images,sku_color_group,url,price_range,sale_price_range,description,is_live,score&fq=sale_price:%5B8+TO+17%5D&realm=staging&br_origin=searchbox&l=red+shirt&q=red+shirts&search_type=keyword&stats.field=sale_price"))
+        .get(session => "/api/v1/core/?account_id=6063&auth_key=&domain_key=oldnavy&request_id=7839579868130&_br_uid_2=uid=7596393004555:_uid=5727955944386:v=11.8:ts=1480613947786:hc=108&url=www.bloomique.com&ref_url=www.bloomique.com&request_type=search&rows=200&start=0&facet.limit=300&fl=pid,title,brand,price,sale_price,promotions,thumb_image,sku_thumb_images,sku_swatch_images,sku_color_group,url,price_range,sale_price_range,description,is_live,score&fq=sale_price:%5B8+TO+17%5D&realm=staging&br_origin=searchbox&l=red+shirt&q=red+shirts&search_type=keyword&stats.field=sale_price")
+        .check(jsonPath("$..numFound").ofType[Int].greaterThan(0)))
     }
 
   setUp(
