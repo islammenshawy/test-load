57010e5d32f492f5ac0f8a85450ed11e32ae9b9f
ielmens <islam_elmenshawy@gap.com>
2017-08-30 13:32:57 -0700

    FND-16 Added brand to the variables configured by build

--- src/gatling/scala/PeakLoad.scala
+++ src/gatling/scala/PeakLoad.scala
@@ -6,17 +6,20 @@ import scala.util.Properties.envOrElse
 import scala.concurrent.duration._
 
 class PeakLoad extends Simulation {
+  val brandAccountMap = scala.collection.mutable.Map("ON" -> "6063")
 
   val baseUrl = envOrElse("TARGET_URL", "http://brm-staging-core-0.brsrvr.com")
+  val brand = envOrElse("BRAND_PARAM", "ON")
+  val httpConf = http.baseURL(baseUrl)
 
-  val httpConf = http
-    .baseURL(baseUrl)
+  val httpUrlNoBase = "/api/v1/core/?account_id=%s&auth_key=&domain_key=oldnavy&request_id=7839579868130&_br_uid_2=uid=7596393004555:_uid=5727955944386:v=11.8:ts=1480613947786:hc=108&url=www.bloomique.com&ref_url=www.bloomique.com&request_type=search&rows=200&start=0&facet.limit=300&fl=pid,title,brand,price,sale_price,promotions,thumb_image,sku_thumb_images,sku_swatch_images,sku_color_group,url,price_range,sale_price_range,description,is_live,score&fq=sale_price:%5B8+TO+17%5D&realm=staging&br_origin=searchbox&l=red+shirt&q=red+shirts&search_type=keyword&stats.field=sale_price"
+  val httpUrlWithParam = httpUrlNoBase.format(brandAccountMap(brand))
 
 
   val scn = scenario("PeakLoad")
     .group("Request") {
       exec(http("search page")
-        .get(session => "/api/v1/core/?account_id=6063&auth_key=&domain_key=oldnavy&request_id=7839579868130&_br_uid_2=uid=7596393004555:_uid=5727955944386:v=11.8:ts=1480613947786:hc=108&url=www.bloomique.com&ref_url=www.bloomique.com&request_type=search&rows=200&start=0&facet.limit=300&fl=pid,title,brand,price,sale_price,promotions,thumb_image,sku_thumb_images,sku_swatch_images,sku_color_group,url,price_range,sale_price_range,description,is_live,score&fq=sale_price:%5B8+TO+17%5D&realm=staging&br_origin=searchbox&l=red+shirt&q=red+shirts&search_type=keyword&stats.field=sale_price")
+        .get(session => httpUrlWithParam)
         .check(jsonPath("$..numFound").ofType[Int].greaterThan(0)))
     }
 
